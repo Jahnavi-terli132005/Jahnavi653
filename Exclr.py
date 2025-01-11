5c09a335c34cf6cf8d0a7e3ceb2e3164514123a5
Jahnavi-terli132005 <2311CS020653@mallareddyuniversity.ac.in>
2025-01-11 05:28:43 +0000

    String Splitting

---
+++ Exclr.py
@@ -0,0 +1,8 @@
+string="do what i say to the world"
+"""for word in string.split():
+    if len(word)%2==0:
+        print(word)
+    if len(word)>3:
+        print(word[0])"""
+x=list(string.split())
+print(x)
